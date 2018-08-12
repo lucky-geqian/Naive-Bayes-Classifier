ff750c08011db023e03200435e79006ab081ed3b
lucky-geqian <406057971@qq.com>
2018-08-12 20:17:31 +0800

    修改适配

--- scikit-learn nb classifier.py
+++ scikit-learn nb classifier.py
@@ -2,6 +2,7 @@
 import sys
 
 import jieba
+import os
 import numpy
 from sklearn import metrics
 from sklearn.feature_extraction.text import HashingVectorizer
@@ -9,16 +10,18 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 def input_data(train_file, test_file):
+    folder_path = 'E:/github/Naive-Bayes-Classifier/Database/SogouC/Shywl'
+    folder_list = os.listdir(folder_path)
     train_words = []
     train_tags = []
     test_words = []
     test_tags = []
-    with open(train_file, 'r') as f1:
+    with open(train_file, 'r', encoding='UTF-8') as f1:
         for line in f1:
             tks = line.split('\t', 1)
             train_words.append(tks[1])
             train_tags.append(tks[0])
-    with open(test_file, 'r') as f1:
+    with open(test_file, 'r', encoding='UTF-8') as f1:
         for line in f1:
             tks = line.split('\t', 1)
             test_words.append(tks[1])
@@ -26,7 +29,7 @@ def input_data(train_file, test_file):
     return train_words, train_tags, test_words, test_tags
 
 
-with open('stopwords.txt', 'r') as f:
+with open('E:/github/Naive-Bayes-Classifier/stopwords_cn.txt', 'r', encoding='UTF-8') as f:
     stopwords = set([w.strip() for w in f])
 comma_tokenizer = lambda x: jieba.cut(x, cut_all=True)
 
@@ -39,8 +42,9 @@ def vectorize(train_words, test_words):
 
 
 def evaluate(actual, pred):
-    m_precision = metrics.precision_score(actual, pred)
-    m_recall = metrics.recall_score(actual, pred)
+    m_precision = metrics.precision_score(actual, pred, labels=None, pos_label=1, average='micro', sample_weight=None)
+    m_recall = metrics.recall_score(actual, pred, labels=None, pos_label=1, average='micro',
+                 sample_weight=None)
     print('precision:{0:.3f}'.format(m_precision))
     print('recall:{0:0.3f}'.format(m_recall))
 
@@ -52,11 +56,11 @@ def train_clf(train_data, train_tags):
 
 
 def main():
-    if len(sys.argv) < 3:
-        print('[Usage]: python classifier.py train_file test_file')
-        sys.exit(0)
-    train_file = sys.argv[1]
-    test_file = sys.argv[2]
+    # if len(sys.argv) < 3:
+    #     print('[Usage]: python classifier.py train_file test_file')
+    #     sys.exit(0)
+    train_file = 'E:/github/Naive-Bayes-Classifier/scripts/train_data.txt'
+    test_file = 'E:/github/Naive-Bayes-Classifier/scripts/test_data.txt'
     train_words, train_tags, test_words, test_tags = input_data(train_file, test_file)
     train_data, test_data = vectorize(train_words, test_words)
     clf = train_clf(train_data, train_tags)
